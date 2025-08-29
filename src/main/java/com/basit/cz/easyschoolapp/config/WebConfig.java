69bf7ab6234f86d71e709decb35194c95fec862f
Abdul Basit <tcomprog@gmail.com>
2025-08-29 22:09:37 +0200

    added new pages

---
+++ src/main/java/com/basit/cz/easyschoolapp/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.basit.cz.easyschoolapp.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/contact").setViewName("contact");
+        registry.addViewController("/courses").setViewName("courses");
+        registry.addViewController("/about").setViewName("about");
+    }
+}
--- src/main/java/com/basit/cz/easyschoolapp/controller/HomeController.java
+++ src/main/java/com/basit/cz/easyschoolapp/controller/HomeController.java
@@ -4,14 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
 @Controller
 public class HomeController {
 
     @GetMapping({"","/","/home"})
-    public String home(Model model) {
-        model.addAttribute("country", List.of("Pakistan","India","Japan","China"));
+    public String home() {
         return "home";
     }
 
