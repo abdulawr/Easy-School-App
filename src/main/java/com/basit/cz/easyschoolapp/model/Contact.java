cb14eb6e5a13dc71b60cc2e69696b862b67b558b
Abdul Basit <tcomprog@gmail.com>
2025-08-29 23:43:02 +0200

    small changes

--- src/main/java/com/basit/cz/easyschoolapp/model/Contact.java
+++ src/main/java/com/basit/cz/easyschoolapp/model/Contact.java
@@ -1,15 +1,10 @@
 package com.basit.cz.easyschoolapp.model;
 
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString
 public class Contact {
     String name,mobileNum, email,subject,message;
-
-
-    @Override
-    public String toString() {
-        return "Contact [name=" + name + ", email=" + email + ", mobileNum=" +
-                mobileNum + ", subject=" + subject + ", message=" + message + "]";
-    }
 }
